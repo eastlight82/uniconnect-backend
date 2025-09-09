b1686d65cbf2b5d57bd9b370ef769e457b7e880f
eastlight82 <bigstake82@gmail.com>
2025-09-09 15:23:13 +0900

    feat start pjct

---
+++ src/test/java/com/uni_connect/UniConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
